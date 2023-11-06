3208ca9cfb89f72bc9597f1da66ed1f9dc964cec
Tony <123456789@user.noreply.gmail.com>
2023-11-07 01:50:35 +0800

    edit

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ window.onclick = function(event) {
       }
     }
   }
+
+function popUpWindow(){
+    // Get the elements by their ID
+    var popupLink = document.getElementById("popup-link");
+    var popupWindow = document.getElementById("popup-window");
+    var closeButton = document.getElementById("close-button");
+      // Show the pop-up window when the link is clicked
+    popupLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+      });
+      // Hide the pop-up window when the close button is clicked
+    closeButton.addEventListener("click", function() {
+        popupWindow.style.display = "none";
+    });
+
+}
